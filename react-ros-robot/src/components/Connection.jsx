91d0023debf8f4cfd45b616ff6a00c762283fb93
ehvenimunis <muhammedsalihaydogan@gmail.com>
2021-04-25 19:02:55 +0300

    Connected companenet added

--- react-ros-robot/src/components/Connection.jsx
+++ react-ros-robot/src/components/Connection.jsx
@@ -1,6 +1,55 @@
+import { thisExpression } from '@babel/types';
+import React, {Component } from 'react';
+import Alert from 'react-bootstrap/Alert';
 
 
 class Connection extends Component {
+    state = {
+        connected: false,
+        ros: null
+    }
+
+    constructor(){
+        super();
+        this.init_connection();
+    }
+
+    init_connection(){
+        this.state.ros = new window.ROSLIB.Ros();
+        console.log(this.state.ros);
+        
+        this.state.ros.on("connection", () => {
+            console.log("connection established!");
+            this.setState({ connected: true});
+
+        });
+
+        this.state.ros.on("close", () => {
+            console.log("connection closed!");
+            this.setState({ connected: false});
+
+        });
+
+        try{
+            this.state.ros.connect("ws://192.168.8.101:9090");
+        }catch (error){
+            console.log("connection problem");
+        }
+
+
+        
+    }
+
+    render() {
+        return (
+            <div>
+                <Alert className="text-center m-3" variant={this.state.connected ? "success": "danger"}>
+                    {this.state.connected? "Robot connected" : "Robot disconnected"}
+                </Alert>
+                
+            </div>
+        );
+    }
   
 }
 
--- react-ros-robot/src/components/Home.jsx
+++ react-ros-robot/src/components/Home.jsx
@@ -1,44 +1,19 @@
-import { thisExpression } from "@babel/types";
 import React, {Component} from "react";
-import { Button } from "react-bootstrap"
+import Connection from "./Connection";
+
+
 
 class Home extends Component {
     state = {
-        counter: 1,
+        
     };
-
-    increment_counter(){
-        //this.state.counter = this.state.counter + 1;
-        this.setState({counter: this.state.counter + 1});
-        console.log(this.state.counter);
-    }
-
     render(){
         return (
         <main>  
             <h1 className="text-center mt-3">  Robot Control Page</h1>
 
-            <h5>
-                Counter:
-                <span>{this.state.counter}</span>
-            </h5>
-            <Button onClick={() => this.increment_counter()}>Increment</Button>
-            
-
-
-        
-        
-        
-        
-        
+            <Connection ></Connection>
         </main>
-
-        
-
-    
-
-
-
         );
     }
 
