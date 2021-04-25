dd5a109a127657b45a787654a6a10fe5aae93f9a
ehvenimunis <muhammedsalihaydogan@gmail.com>
2021-04-25 17:57:14 +0300

    State consept understood

--- react-ros-robot/src/components/Home.jsx
+++ react-ros-robot/src/components/Home.jsx
@@ -1,11 +1,44 @@
+import { thisExpression } from "@babel/types";
 import React, {Component} from "react";
+import { Button } from "react-bootstrap"
 
 class Home extends Component {
-    state = {};
+    state = {
+        counter: 1,
+    };
+
+    increment_counter(){
+        //this.state.counter = this.state.counter + 1;
+        this.setState({counter: this.state.counter + 1});
+        console.log(this.state.counter);
+    }
 
     render(){
         return (
-        <main> <h1>Home</h1></main>
+        <main>  
+            <h1 className="text-center mt-3">  Robot Control Page</h1>
+
+            <h5>
+                Counter:
+                <span>{this.state.counter}</span>
+            </h5>
+            <Button onClick={() => this.increment_counter()}>Increment</Button>
+            
+
+
+        
+        
+        
+        
+        
+        </main>
+
+        
+
+    
+
+
+
         );
     }
 
