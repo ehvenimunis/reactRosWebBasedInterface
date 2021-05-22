9fd2f812ba8123b96f943fcc774e04c8c5f10c2c
ehvenimunis <muhammedsalihaydogan@gmail.com>
2021-05-22 18:02:50 +0300

    Added nav2d and ros2d js file for 2D map

--- react-ros-robot/src/components/Navigation.jsx
+++ react-ros-robot/src/components/Navigation.jsx
@@ -1,109 +1,54 @@
 import React, { Component } from "react";
-import { Col, Container, Row, Button } from "react-bootstrap"
-import Config from '../scripts/config';
-
-var navigation = false;
-var pathed = false;
-var homing = false;
-var MAP_WIDTH = (window.innerWidth)*0.65;
-var MAP_HEIGHT = window.innerHeight - (window.innerHeight)*0.08;
+import NAV2D from './vendor/nav2d.js'
+import ROS2D from './vendor/ros2d.js'
+import PropTypes from 'prop-types';
+import ROSLIB from 'roslib'
 
 class Navigation extends Component {
-    state = {
-        ros: null, 
-    };
 
-    constructor(){
-        super();
-        this.init_connection();
-        this.handleOpen = this.handleOpen.bind(this);
-        this.handleClose = this.handleClose.bind(this);
-    };
-
-    init_connection(){
-        this.state.ros = new window.ROSLIB.Ros();
-        console.log(this.state.ros);
-
-        this.state.ros.on("connection", () => {
-            console.log("connection established in Teleoperation Companenet!");
-            console.log(this.state.ros);
-            this.setState({ connected: true});
-
+    componentDidMount(){
+        const ros = this.props.ros;
+        const viewer = new ROS2D.Viewer({
+          divID : this.props.id,
+          width : this.props.width,
+          height : this.props.height
         });
-
-        this.state.ros.on("close", () => {
-            console.log("connection closed!");
-            this.setState({ connected: false});
-
-            //try to reconnect every 3 seconds
-            setTimeout(() => {
-                try{
-                    this.state.ros.connect("ws://"+
-                    Config.ROSBRIDGE_SERVER_IP+":"+
-                    Config.ROSBRIDGE_SERVER_PORT+""
-                    );
-                }catch (error){
-                    console.log("connection problem");
-                }
-            }, Config.RECONNECTION_TIMER);
-
+        const nav = NAV2D.OccupancyGridClientNav({
+          ros : ros,
+          rootObject : viewer.scene,
+          viewer : viewer,
+          topic: this.props.topic,
+          serverName : this.props.serverName
         });
-        try{
-            this.state.ros.connect("ws://"+
-            Config.ROSBRIDGE_SERVER_IP+":"+
-            Config.ROSBRIDGE_SERVER_PORT+""
-            );
-        }catch (error){
-            console.log("connection problem");
-        }
-    }
-
-    // methods
-    handleOpen() {
-        console.log("hanle open");
-
-        var relay_pub = new window.ROSLIB.Topic({
-            ros: this.state.ros,
-            name: "/r5/open_relay",
-            messageType: 'std_msgs/Bool',
-          });
-
-          var str = new window.ROSLIB.Message({
-            data : true
-          });
-
-
-        relay_pub.publish(str);
-
-    }
-
-    handleClose() {
-        console.log("hanle close");
-
-        var relay_pub = new window.ROSLIB.Topic({
-            ros: this.state.ros,
-            name: "/r5/open_relay",
-            messageType: 'std_msgs/Bool',
-          });
-
-          var str = new window.ROSLIB.Message({
-            data : false
-          });
-
-
-        relay_pub.publish(str);
     }
 
     render() {
         return(
             <>
-                <Button variant="warning" onClick={this.handleOpen}>Zoom In</Button>{' '}
-                <Button variant="warning" onClick={this.handleClose}>Zoom Out</Button>{' '}
+                return <div id={this.props.id}/>
             </>
         );
     }
-
-
 }
+
+Navigation.defaultProps = {
+    ros: new ROSLIB.Ros({
+      url : 'ws://localhost:9090'
+    }),
+    id: 'nav2d',
+    width: 500,
+    height: 500,
+    topic: '/map',
+    serverName: '/move_base'
+  };
+  
+Navigation.propTypes = {
+    ros: PropTypes.object,
+    id: PropTypes.string,
+    width: PropTypes.number,
+    height: PropTypes.number,
+    topic: PropTypes.string,
+    serverName: PropTypes.string
+  }
 
 export default Navigation;
